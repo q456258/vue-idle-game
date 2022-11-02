271f0d31ad98feb41dd4506fb72de78e8f3ea293
q456258 <343670178@qq.com>
2022-11-02 19:44:13 -0400

    部分css移到main

--- src/views/component/equipEnhance.vue
+++ src/views/component/equipEnhance.vue
@@ -1,5 +1,5 @@
 <template>
-<draggable class="equip">
+<draggable class="equipPanel">
     <template slot="header">
     </template>
     <template slot="main" >
@@ -171,108 +171,6 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.equip {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
-    height: 28rem;
-    width: 48rem;
-    background-image: url("/icons/ui/enhancePanel2.png");
-    background-repeat: no-repeat;
-    background-size: 49rem 28rem;
-    z-index: 10;
-    .title {
-        position: absolute;
-        top: 1.4rem;
-        left: 0;
-        right: 0;
-        font-weight: bold;
-        font-size: 1.5rem;
-    }
-    .info {
-        position: absolute;
-        width: 50%;
-        .name {
-            position: relative;
-            top: 11rem;
-            left: 2.5rem;
-        }
-        .largeIconContainer {
-            position: relative;
-            top: 12rem;
-            left: 2.5rem;
-        }
-        .enhanceLv {
-            position: relative;
-            top: 200px;
-            left: 35px;
-            .progress {
-                width: 200px;
-                margin: auto;
-                height: 3px;
-            }
-        }
-    }
-    .enhance {
-        position: absolute;
-        margin-left: 55%;
-        margin-top: 10%;
-        width: 50%;
-        height: 55%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        .beforeEnhance {
-            margin-left: 1rem;
-            text-align: left;
-            .bonus {
-                color: #0f0;
-            }
-        }
-    }
-    .warning {
-        color: #D8000C;
-    }
-    .confirm {
-        position: absolute;
-        top: 21rem;
-        left: 35rem;
-        height: 2.5rem;
-        width: 8rem;
-        background-image: url("/icons/ui/button.png");
-        background-repeat: no-repeat;
-        background-size: 8rem 2.5rem;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 1.3rem;
-        opacity: 0.8; 
-        &:hover {
-            opacity: 1; 
-        }
-    }
-    .cancel {
-        position: absolute;
-        top: 24rem;
-        left: 35rem;
-        height: 2.5rem;
-        width: 8rem;
-        background-image: url("/icons/ui/button.png");
-        background-repeat: no-repeat;
-        background-size: 8rem 2.5rem;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 1.3rem;
-        opacity: 0.8; 
-        &:hover {
-            opacity: 1; 
-        }
-    }
-}
 .addonGrid {
     position: absolute;
     margin-top: 50%;
