66331d3dfe265f666505c0b5e543f87896c205b5
q456258 <343670178@qq.com>
2023-06-03 21:01:19 -0400

    副本快捷键添加条件，打字框内按下不触发

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@
 export default {
 mounted() {    
     this._keyListener = function(e) {
+        let targetTagName = (e.target.nodeType == 1) ? e.target.nodeName.toUpperCase() : "";
+        if (["INPUT", "SELECT", "TEXTAREA"].indexOf(targetTagName) != -1)
+            return
         let index = this.$store.globalComponent['index'];  
         if (e.key === "b") {
             index.openMenuPanel('backpack')
@@ -19,7 +22,8 @@ mounted() {
         } else if (e.key === "q") {
             index.openMenuPanel('quest')
         } else if (e.key === "d") {
-            index.openMenuPanel('dungeon')
+            if(this.$store.state.guildAttribute.smith.lv >= 1)
+                index.openMenuPanel('dungeon')
         } else if (e.key === "c") {
             if(this.$store.state.playerAttribute.lv >= 20)
                 index.openMenuPanel('craft')
