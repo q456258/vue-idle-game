d78f2c1c190f821e49ed156bf99e031ee7b06024
q456258 <343670178@qq.com>
2023-10-28 19:45:48 -0400

    公会成员tab改为酒馆大于0级后出现

--- src/views/index.vue
+++ src/views/index.vue
@@ -25,7 +25,7 @@
         <a class="nav-link" :class="{active: displayPage=='guild' }" id="guild" @click="switchTab('guild')" v-show="playerLv >= 10">公会</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" :class="{active: displayPage=='guildMember' }" id="guildMember" @click="switchTab('guildMember')" v-show="playerLv >= 20">公会成员</a>
+        <a class="nav-link" :class="{active: displayPage=='guildMember' }" id="guildMember" @click="switchTab('guildMember')" v-show="guild.bar.lv > 0">公会成员</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" :class="{active: displayPage=='shop' }" id="shop" @click="switchTab('shop')" v-show="guild.shop.lv > 0">商店</a>
