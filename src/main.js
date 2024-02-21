314c6a57e4f678b03379f5a3fffede337fe042f2
q456258 <343670178@qq.com>
2024-02-21 12:49:09 -0500

    调整css优先级

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/assets/css/base.scss'
-import '@/assets/css/main.scss'
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import '@/assets/css/base.scss'
+import '@/assets/css/main.scss'
 
 // import {
 //   getAxiosInstance
